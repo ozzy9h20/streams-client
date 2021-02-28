46e774f72e3c930c160f4dbcdfec5dbada0ad983
Mochammad Endyarroziqin <mochiaria97@gmail.com>
2021-03-01 01:03:21 +0700

    Add Simple Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
-        <Route path="/" exact component={StreamList} />
-        <Route path="/streams/new" component={StreamCreate} />
-        <Route path="/streams/edit" component={StreamEdit} />
-        <Route path="/streams/delete" component={StreamDelete} />
-        <Route path="/streams/show" component={StreamShow} />
+        <div>
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/delete" component={StreamDelete} />
+          <Route path="/streams/show" component={StreamShow} />
+        </div>
       </BrowserRouter>
     </div>
   );
