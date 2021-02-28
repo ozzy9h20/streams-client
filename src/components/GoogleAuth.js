1b99d2ebc1a3a4ebb770754509f3c209734fb844
Mochammad Endyarroziqin <mochiaria97@gmail.com>
2021-03-01 02:01:02 +0700

    Add Click Auth Handler

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -23,20 +23,48 @@ class GoogleAuth extends React.Component {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   }
 
+  onSignIn = () => {
+    this.auth.signIn();
+  }
+
+  onSignOut = () => {
+    this.auth.signOut();
+  }
+
   renderAuthButton = () => {
     if (this.state.isSignedIn === null) {
-      return <div>I dont know if we are signed in</div>
+      return (
+        <button className="ui loading button">
+          Loading..
+        </button>  
+      );
     }
     else if (this.state.isSignedIn) {
-      return <div>I am signed in!</div>
+      return (
+        <button 
+          className="ui red google button"
+          onClick={this.onSignOut}
+        >
+          <i className="google icon" />
+          Sign Out
+        </button>  
+      );
     }
     else {
-      return <div>I am not signed in</div>
+      return (
+        <button 
+          className="ui blue google button"
+          onClick={this.onSignIn}
+          >
+          <i className="google icon" />
+          Sign In
+        </button>  
+      );
     }
   }
 
   render() {
-    return <div>{this.renderAuthButton()}</div>
+    return <div className="item">{this.renderAuthButton()}</div>
   }
 }
 
