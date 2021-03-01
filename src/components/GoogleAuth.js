7c78dc9abfc2844f0c4c11597cc6141749b7f14d
Mochammad Endyarroziqin <mochiaria97@gmail.com>
2021-03-01 07:02:12 +0700

    Redux Init

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -23,11 +23,11 @@ class GoogleAuth extends React.Component {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   }
 
-  onSignIn = () => {
+  onSignInClick = () => {
     this.auth.signIn();
   }
 
-  onSignOut = () => {
+  onSignOutClick = () => {
     this.auth.signOut();
   }
 
@@ -43,7 +43,7 @@ class GoogleAuth extends React.Component {
       return (
         <button 
           className="ui red google button"
-          onClick={this.onSignOut}
+          onClick={this.onSignOutClick}
         >
           <i className="google icon" />
           Sign Out
@@ -54,7 +54,7 @@ class GoogleAuth extends React.Component {
       return (
         <button 
           className="ui blue google button"
-          onClick={this.onSignIn}
+          onClick={this.onSignInClick}
           >
           <i className="google icon" />
           Sign In
