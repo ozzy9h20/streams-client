6c52360f3ff6e74ff65df97f5af170e7fd2c8853
Mochammad Endyarroziqin <mochiaria97@gmail.com>
2021-03-02 07:03:32 +0700

    Styling Modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import history from '../history';
 
-const Modal = (props) => {
+const Modal = ({ title, content, actions }) => {
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">
-        Modal
+    <div 
+      onClick={() => history.push('/')} 
+      className="ui dimmer modals visible active"
+    >
+      <div 
+        onClick={(e) => e.stopPropagation()}
+        className="ui standard modal visible active"
+      >
+        <div className="header">{title}</div>
+        <div className="content">{content}</div>
+        <div className="actions">{actions}</div>
       </div>
     </div>,
     document.getElementById('modal')
