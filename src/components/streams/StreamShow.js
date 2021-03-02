42e5ead4fb6ac76c587c1560698b95f7659658cd
Mochammad Endyarroziqin <mochiaria97@gmail.com>
2021-03-02 08:19:59 +0700

    [FIX] Route + Connect StreamShow

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from './../../actions/index';
 
-const StreamShow = () => {
-  return ( 
-    <div>StreamShow</div>
+const StreamShow = ({ fetchStream, match, stream}) => {
+  useEffect(() => {
+    fetchStream(match.params.id);
+  }, []);
+
+  if (!stream) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <div>
+      <h1>{stream?.title}</h1>
+      <h3>{stream?.description}</h3>
+    </div>
    );
 }
- 
-export default StreamShow;
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  { fetchStream }
+)(StreamShow);
